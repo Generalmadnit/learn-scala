25d2daf3f207123fb91904be3db36a32cddc852e
generalmadnit <pathurinitin@gmail.com>
2023-08-18 21:43:57 +0530

    18/8/23

---
+++ learn/src/main/scala/Day14.scala
@@ -0,0 +1,29 @@
+import scala.io.StdIn.readInt
+
+object Day14 {
+  def main(args: Array[String]):Unit=
+    {
+      println("Enter the starting and ending range of numbers")
+      var strt = readInt()
+      var rng = readInt()
+      var cnt = 0
+      var pcmt = 0
+      for(i <- strt to rng)
+      {
+        cnt = 0
+        for(j <- 1 to i)
+          {
+//            println(rng%j)
+            if(i % j ==0)
+              cnt = cnt + 1
+          }
+          if(cnt==2)
+            {
+              println(i)
+              pcmt = pcmt+1
+            }
+            println(i)
+      }
+      println(pcmt," Primes")
+    }
+}
