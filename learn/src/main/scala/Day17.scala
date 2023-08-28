e74813b62d9097a32291d8ffc3c689c4aa398a7f
generalmadnit <pathurinitin@gmail.com>
2023-08-28 21:59:36 +0530

    28/8/23

---
+++ learn/src/main/scala/Day17.scala
@@ -0,0 +1,22 @@
+import scala.io.StdIn.readInt
+
+object Day17 {
+  def main(args: Array[String]):Unit=
+    {
+      println("Enter the number to find factorial")
+      var end = readInt()
+      var fact = 1
+      for(i <- 1 to end)
+      {
+        fact = fact * i
+      }
+      println(fact," is the factorial")
+      var r = List[Int](1,9,5,7,6,8,4,3,2)
+      println(r.sorted+" sorted list using inbuilt functions")
+      r = r.sorted
+      for(i <- r)
+        {
+          println(i)
+        }
+    }
+}
